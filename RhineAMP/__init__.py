cd6e0a403045cf9124b40c620866c94b735b6641
Yoxiro <yukinoyou@outlook.com>
2024-02-26 19:50:54 +0800

    PseAAC method part 2

--- RhineAMP/__init__.py
+++ RhineAMP/__init__.py
@@ -1,2 +1,3 @@
 from RhineAMP.Descriptors.AAC import AAC
 from RhineAMP.Descriptors.DPC import DPC
+from RhineAMP.Descriptors.PseAAC import PseAAC
--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import RhineAMP
+import RhineAMP.Descriptors.PseAAC
 LL_37 = "LLGDFFRKSKEKIGKEFKRIVQRIKDFLRNLVPRTESA"
 
-from RhineAMP.Descriptors.PseAAC import normalized_aac
-
-nor = normalized_aac(LL_37)
-std = nor.std(ddof=0)
-mean = nor.mean()
-print(nor)
-print(std)
-print(mean)
