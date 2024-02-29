19582234c0260a0dea821bf8aebeb1ca3630c9fb
Yoxiro <yukinoyou@outlook.com>
2024-02-29 16:21:47 +0800

    Fastaread,FEGS added

--- RhineAMP/__init__.py
+++ RhineAMP/__init__.py
@@ -1,3 +1,5 @@
 from RhineAMP.Descriptors.AAC import AAC
 from RhineAMP.Descriptors.DPC import DPC
 from RhineAMP.Descriptors.PseAAC import PseAAC,PseAAC_Amphiphilic
+from RhineAMP.Descriptors.FEGS import FEGS
+from RhineAMP.FastaRead import FastaRead
