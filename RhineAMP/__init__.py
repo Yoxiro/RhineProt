24865e44f94fc54f4ae04a5279c872553c1bae6f
Yoxiro <yukinoyou@outlook.com>
2024-03-22 19:37:06 +0800

    ML

--- RhineAMP/__init__.py
+++ RhineAMP/__init__.py
@@ -3,6 +3,9 @@ from RhineAMP.Descriptors.DPC import DPC
 from RhineAMP.Descriptors.PseAAC import PseAAC,PseAAC_Amphiphilic
 from RhineAMP.Descriptors.FEGS import FEGS
 from RhineAMP.Descriptors.BLOSSUM62 import BLOSUM_62,BLOSUM_62_fasta
+from RhineAMP.Descriptors.CKSAAP import CKSAAP
+
+
 from RhineAMP.FastaRead import FastaRead
 from RhineAMP.FastaClean import FastaClean
 from RhineAMP.FastaSave import FastaSave
@@ -10,3 +13,4 @@ from RhineAMP.FastaFilter import FastaFilter
 from RhineAMP.SaveAfterRun import SaveAfterRun
 from RhineAMP.Sequences import Sequences
 from RhineAMP.FastaPadding import FastaPadding
+
